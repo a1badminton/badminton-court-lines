c700ee8a8e1aa6ad36d454bad5635f4f1db5cb5c
Anthon Pang <anthonp@nationalfibre.net>
2025-10-31 18:30:38 -0400

    refactor to translate origin + dimensions to x, y, width, height

--- src/badminton-court.php
+++ src/badminton-court.php
@@ -19,166 +19,112 @@ define('SERVICE_COURT_LENGTH', 3880);
 define('SIDE_ALLEY_WIDTH', 420); // between the singles and doubles sidelines
 define('BACK_ALLEY_WIDTH', 720); // between the singles and doubles long service lines
 
-// badminton court rendered in portrait orientation, where [x, y] is relative to the top-left corner
-$data = [
+// badminton court rendered in portrait orientation, where [x, y] is relative to the top-left corner;
+// only half of the lines, service courts, and speed test markers are specified here;
+// we use code to mirror the other half
+$source = [
     "court" => [
-        // court width => 6100 mm or 20'; court length: 13400 mm or 44'
+        // [x, y]
         "origin" => [0, 0],
-        "dimensions" => [COURT_WIDTH, COURT_LENGTH],
 
-        // [x1,y1] to [x2,y2]
-        "x1" => 0,
-        "y1" => 0,
-        "x2" => COURT_WIDTH,
-        "y2" => COURT_LENGTH,
+        // court width => 6100 mm or 20'; court length: 13400 mm or 44'
+        "dimensions" => [COURT_WIDTH, COURT_LENGTH],
     ],
 
     "net" => [
         "origin" => [0, COURT_LENGTH / 2],
         "dimensions" => [COURT_WIDTH, 0],
-        "x1" => 0,
-        "y1" => COURT_LENGTH / 2,
-        "x2" => COURT_WIDTH,
-        "y2" => COURT_LENGTH / 2,
     ],
 
     "courtLines" => [
         // aka singles long service line, or baseline
         "back boundary line" => [
-            [
-                "origin" => [0, 0],
-                "dimensions" => [COURT_WIDTH, LINE_WIDTH],
-                "x1" => 0,
-                "y1" => 0,
-                "x2" => COURT_WIDTH,
-                "y2" => LINE_WIDTH, 
-            ],
+            "origin" => [0, 0],
+            "dimensions" => [COURT_WIDTH, LINE_WIDTH],
         ],
         "doubles sideline" => [
-            [
-                "origin" => [0, 0],
-                "dimensions" => [LINE_WIDTH, COURT_LENGTH],
-                "x1" => 0,
-                "y1" => 0,
-                "x2" => LINE_WIDTH,
-                "y2" => COURT_LENGTH
-            ],
+            "origin" => [0, 0],
+            "dimensions" => [LINE_WIDTH, COURT_LENGTH],
         ],
         "singles sideline" => [
-            [
-                "origin" => [LINE_WIDTH + SIDE_ALLEY_WIDTH, 0],
-                "dimensions" => [LINE_WIDTH, COURT_LENGTH],
-                "x1" => 460,
-                "y1" => 0,
-                "x2" => 500,
-                "y2" => COURT_LENGTH
-            ],
+            "origin" => [LINE_WIDTH + SIDE_ALLEY_WIDTH, 0],
+            "dimensions" => [LINE_WIDTH, COURT_LENGTH],
         ],
         "centre line" => [
-            [
-                "origin" => [LINE_WIDTH + SIDE_ALLEY_WIDTH + LINE_WIDTH + SERVICE_COURT_WIDTH, 0],
-                "dimensions" => [LINE_WIDTH, LINE_WIDTH + BACK_ALLEY_WIDTH + LINE_WIDTH + SERVICE_COURT_LENGTH + LINE_WIDTH],
-                "x1" => 3030,
-                "y1" => 0,
-                "x2" => 3070,
-                "y2" => 4720
-            ],
+            "origin" => [LINE_WIDTH + SIDE_ALLEY_WIDTH + LINE_WIDTH + SERVICE_COURT_WIDTH, 0],
+            "dimensions" => [LINE_WIDTH, LINE_WIDTH + BACK_ALLEY_WIDTH + LINE_WIDTH + SERVICE_COURT_LENGTH + LINE_WIDTH],
         ],
         "doubles long service line" => [
-            [
-                "origin" => [0, LINE_WIDTH + BACK_ALLEY_WIDTH],
-                "dimensions" => [COURT_WIDTH, LINE_WIDTH],
-                "x1" => 0,
-                "y1" => 760,
-                "x2" => COURT_WIDTH,
-                "y2" => 800
-            ],
+            "origin" => [0, LINE_WIDTH + BACK_ALLEY_WIDTH],
+            "dimensions" => [COURT_WIDTH, LINE_WIDTH],
         ],
         "short service line" => [
-            [
-                "origin" => [0, LINE_WIDTH + BACK_ALLEY_WIDTH + LINE_WIDTH + SERVICE_COURT_LENGTH],
-                "dimensions" => [COURT_WIDTH, LINE_WIDTH],
-                "x1" => 0,
-                "y1" => 4680,
-                "x2" => COURT_WIDTH,
-                "y2" => 4720
-            ],
+            "origin" => [0, LINE_WIDTH + BACK_ALLEY_WIDTH + LINE_WIDTH + SERVICE_COURT_LENGTH],
+            "dimensions" => [COURT_WIDTH, LINE_WIDTH],
         ]
     ],
 
+    // for simplicity, this excludes the side and back alleys
+    // because the service courts vary in dimensions depending on singles vs doubles, and doubles serving vs receiving
     "serviceCourts" => [
         "right service court" => [
-            [
-                "origin" => [LINE_WIDTH + SIDE_ALLEY_WIDTH + LINE_WIDTH, LINE_WIDTH + BACK_ALLEY_WIDTH + LINE_WIDTH],
-                "dimensions" => [SERVICE_COURT_WIDTH, SERVICE_COURT_LENGTH],
-                "x1" => 500,
-                "y1" => 800,
-                "x2" => 3030,
-                "y2" => 4680
-            ],
+            "origin" => [LINE_WIDTH + SIDE_ALLEY_WIDTH + LINE_WIDTH, LINE_WIDTH + BACK_ALLEY_WIDTH + LINE_WIDTH],
+            "dimensions" => [SERVICE_COURT_WIDTH, SERVICE_COURT_LENGTH],
         ],
         "left service court" => [
-            [
-                "origin" => [LINE_WIDTH + SIDE_ALLEY_WIDTH + LINE_WIDTH + SERVICE_COURT_WIDTH + LINE_WIDTH, LINE_WIDTH + BACK_ALLEY_WIDTH + LINE_WIDTH],
-                "dimensions" => [SERVICE_COURT_WIDTH, SERVICE_COURT_LENGTH],
-                "x1" => 3070,
-                "y1" => 800,
-                "x2" => 5600,
-                "y2" => 4680
-            ],
+            "origin" => [LINE_WIDTH + SIDE_ALLEY_WIDTH + LINE_WIDTH + SERVICE_COURT_WIDTH + LINE_WIDTH, LINE_WIDTH + BACK_ALLEY_WIDTH + LINE_WIDTH],
+            "dimensions" => [SERVICE_COURT_WIDTH, SERVICE_COURT_LENGTH],
         ]
     ],
 
+    // markers used to determine if a shuttlecock has the correct flight speed
     "speedTest" => [
         "fast" => [
-            [
-                "origin" => [LINE_WIDTH + SIDE_ALLEY_WIDTH + LINE_WIDTH, 530],
-                "dimensions" => [LINE_WIDTH, LINE_WIDTH],
-                "x1" => 500,
-                "y1" => 530,
-                "x2" => 540,
-                "y2" => 570 
-            ],
+            "origin" => [LINE_WIDTH + SIDE_ALLEY_WIDTH + LINE_WIDTH, 530],
+            "dimensions" => [LINE_WIDTH, LINE_WIDTH],
         ],
         "slow" => [
-            [
-                "origin" => [LINE_WIDTH + SIDE_ALLEY_WIDTH + LINE_WIDTH, 950],
-                "dimensions" => [LINE_WIDTH, LINE_WIDTH],
-                "x1" => 500,
-                "y1" => 950,
-                "x2" => 540,
-                "y2" => 990
-            ],
+            "origin" => [LINE_WIDTH + SIDE_ALLEY_WIDTH + LINE_WIDTH, 950],
+            "dimensions" => [LINE_WIDTH, LINE_WIDTH],
         ]
     ]
 ];
 
+$data = [];
+
+foreach (['court', 'net'] as $type) {
+    $data[$type] = [
+        'x' => $source[$type]['origin'][0],
+        'y' => $source[$type]['origin'][1],
+        'w' => $source[$type]['dimensions'][0],
+        'h' => $source[$type]['dimensions'][1],
+    ];
+}
+
 // generate the counterpart court lines and services by flipping diagonally
 foreach (['courtLines', 'serviceCourts', 'speedTest'] as $type) {
-    foreach ($data[$type] as $name => $area) {
-        $x2 = COURT_WIDTH  - $area[0]['x1'];
-        $y2 = COURT_LENGTH - $area[0]['y1'];
-        $x1 = COURT_WIDTH  - $area[0]['x2'];
-        $y1 = COURT_LENGTH - $area[0]['y2'];
+    $data[$type] = [];
 
-        $width  = $area[0]['dimensions'][0];
-        $height = $area[0]['dimensions'][1];
+    foreach ($source[$type] as $name => $area) {
+        $x = $source[$type][$name]['origin'][0];
+        $y = $source[$type][$name]['origin'][1];
+        $w = $source[$type][$name]['dimensions'][0];
+        $h = $source[$type][$name]['dimensions'][1];
 
-        assert($area[0]['x1'] == $area[0]['origin'][0]);
-        assert($area[0]['y1'] == $area[0]['origin'][1]);
-        assert($x2 - $x1 == $width);
-        assert($y2 - $y1 == $height);
-
-        $mirror = [
-            'origin'     => [$x1, $y1],
-            'dimensions' => $area[0]['dimensions'],
-            'x1'         => $x1,
-            'y1'         => $y1,
-            'x2'         => $x2,
-            'y2'         => $y2,
+        $data[$type][$name] = [];
+        $data[$type][$name][] = [
+            'x' => $x,
+            'y' => $y,
+            'w' => $w,
+            'h' => $h,
         ];
 
-        $data[$type][$name][] = $mirror;
+        $data[$type][$name][] = [
+            'x' => COURT_WIDTH - $x - $w,
+            'y' => COURT_LENGTH - $y - $h,
+            'w' => $w,
+            'h' => $h
+        ];
     }
 }
 
